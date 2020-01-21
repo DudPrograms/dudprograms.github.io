21bd4d14542e996dc4efbc1d7eddd529c7379bb1
Alec Roberts <plstriagin@gmail.com>
2020-01-20 17:38:50 -0800

    added canvas

--- linknext.js
+++ linknext.js
@@ -1,2 +1,7 @@
 var str = "Click here or die!";
 document.write("<p>" + str.link("next.html") + "</p>");
+
+var canvas = document.getElementById("myCanvas");
+var context = canvas.getContext("2d");
+context.fillStyle = "#FF00F0";
+context.fillRect(0, 0, canvas.width, canvas.height);
